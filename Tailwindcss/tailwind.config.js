1388e43a439b527376522114391771b91fc870b8
Lim <3269989624@qq.com>
2025-01-10 21:55:52 +0800

    25.1.10 :tada:

--- Tailwindcss/tailwind.config.js
+++ Tailwindcss/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'jetbrains': ['"JetBrains Mono"']
+      }
+    },
   },
   plugins: [],
 }
